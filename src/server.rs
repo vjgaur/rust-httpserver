3276599857ab0b36490765e14d13bec3d1b96b30
vjgaur <vijayendra.gaur@gmail.com>
2022-04-12 20:23:54 +0530

    handling incoming request using TcpListener

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use std::io::Read;
+use std::net::TcpListener;
+
 pub struct Server {
     addr: String,
 }
@@ -8,5 +11,21 @@ impl Server {
 
     pub fn run(self) {
         println!("Listening on {}", self.addr);
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Receive a request {}", String::from_utf8_lossy(&buffer));
+                        }
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
+                }
+                Err(e) => println!("Failed to establish a connection {}", e),
+            }
+        }
     }
 }

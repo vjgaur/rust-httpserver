fc3512d049f192edb142d4e3c219ea31038183a1
vjgaur <vijayendra.gaur@gmail.com>
2022-04-13 15:22:15 +0530

    implemented request trait

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,6 @@
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 use std::io::Read;
 use std::net::TcpListener;
 
@@ -20,6 +23,10 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Receive a request {}", String::from_utf8_lossy(&buffer));
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {}
+                                Err(e) => println!("Failed to parse a request "),
+                            }
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
                     }

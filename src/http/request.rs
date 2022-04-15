b60127dba225c636f30bacc1a9f1ba6adaf08cf8
vjgaur <vijayendra.gaur@gmail.com>
2022-04-15 07:04:26 +0530

    custome error handling

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,7 +1,10 @@
+use super::convert::TryFrom;
 use super::method::Method;
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
+use std::str;
+use std::str::Utf8Error;
 
 pub struct Request {
     path: String,
@@ -14,7 +17,16 @@ impl TryFrom<&[u8]> for Request {
 
     //Get /Search? name=abc&sort=1 HTTP/1.1
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
-        unimplemented!()
+        match str::from_utf8(buf) {
+            Ok(request) => {}
+            Err(_) => return Err(ParseError::InvalidEncoding),
+        }
+        match str::from_utf8(buf).or(Err(ParseError::InvalidEncoding)) {
+            Ok(request) => {}
+            Err(e) => return Err(e),
+        }
+        let request = str::from_utf8(buf)?;
+        unimplemented!();
     }
 }
 pub enum ParseError {
@@ -34,6 +46,11 @@ impl ParseError {
     }
 }
 
+impl From<Utf8Error> for ParseError {
+    fn from(_: Utf8Error) -> Self {
+        Self::InvalidEncoding
+    }
+}
 impl Display for ParseError {
     fn fmt(&self, f: &mut Formatter) -> FmtResult {
         write!(f, "{}", self.message());

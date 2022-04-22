a1c1c55ec869a16349d687e510413497c0a6d2f8
vjgaur <vijayendra.gaur@gmail.com>
2022-04-22 15:07:18 +0530

    using if-let to optimise code

--- src/http/request.rs
+++ src/http/request.rs
@@ -19,13 +19,20 @@ impl TryFrom<&[u8]> for Request {
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buf)?;
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
             return Err(ParseError::InvalidProtocol);
         }
         let method::Method = method.parse()?;
+        let mut query_string = None;
+
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i + 1..]);
+            path = &path[..i];
+        }
+
         unimplemented!();
     }
 }
